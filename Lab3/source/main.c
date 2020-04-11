ec7ca35559d912a7cd002fb00581eb7230f82350
Raquel Lin <rlin016@wch136-11.cs.ucr.edu>
2020-04-11 15:44:11 -0700

    fixed part 2

--- Lab3/source/main.c
+++ Lab3/source/main.c
@@ -17,25 +17,41 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0x00;
 	DDRB = 0x00; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
+    /* Insert your solution below */
+	unsigned tempA, outC;
 	
-	unsigned char tempA, tempB, cnt;
 	while(1){
-		tempA = PINA;
-		tempB = PINB;
-		cnt = 0x00;
-		while(tempA){
-			if(tempA & 0x01){
-				cnt = cnt + 1;
-			}
-			tempA = tempA >> 1;
+		tempA = PINA & 0x0F;
+		outC = 0x00;
+		switch (tempA){
+			case 15:
+			case 14:
+			case 13:
+				outC = outC | 0x01;
+			case 12:
+			case 11:
+			case 10:
+				outC = outC | 0x02;
+			case 9:
+			case 8:
+			case 7:
+				outC = outC | 0x04;
+			case 6:
+			case 5:
+				outC = outC | 0x08;
+			case 4:
+			case 3:
+				outC = outC | 0x10;
+			case 2:
+			case 1:
+				outC = outC | 0x20;
+				break;
 		}
-		while(tempB){
-			if(tempB & 0x01){
-				cnt = cnt + 1;
-			}
-			tempB = tempB >> 1;
+		if(!(outC & 0x08)){
+			outC = outC | 0x40;
 		}
-		PORTC = cnt;
-	}
+		PORTC = outC;
+	}	
+		
 	return 1;
 }
--- Lab3/turnin/rlin016_lab3_part2.c
+++ Lab3/turnin/rlin016_lab3_part2.c
@@ -17,39 +17,41 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0x00;
 	DDRB = 0x00; PORTB = 0x00;
 	DDRC = 0xFF; PORTC = 0x00;
-    /* Insert your solution below 
- */	while(1){
-	unsigned tempA;
-	tempA = PINA & 0x0F;
-	PORTC = 0x00;
-	switch(tempA){
-		case 15: 
-		case 14:
-		case 13:
-			PORTC = PORTC | 0x01;
-		case 12:
-		case 11:
-		case 10:
-			PORTC = PORTC | 0x02;
-		case 9:
-		case 8:
-		case 7:
-			PORTC = PORTC | 0x04;
-		case 6:
-		case 5:
-			PORTC = PORTC | 0x08;
-		case 4:
-		case 3:
-			PORTC = PORTC | 0x10;
-		case 2:
-		case 1:
-			PORTC = PORTC | 0x20;
-			break;
-	}
-
-	if(!(PORTC & 0x02)){
-		PORTC = PORTC | 0x40;
-	}
-    }
-    return 1;
+    /* Insert your solution below */
+	unsigned tempA, outC;
+	
+	while(1){
+		tempA = PINA & 0x0F;
+		outC = 0x00;
+		switch (tempA){
+			case 15:
+			case 14:
+			case 13:
+				outC = outC | 0x01;
+			case 12:
+			case 11:
+			case 10:
+				outC = outC | 0x02;
+			case 9:
+			case 8:
+			case 7:
+				outC = outC | 0x04;
+			case 6:
+			case 5:
+				outC = outC | 0x08;
+			case 4:
+			case 3:
+				outC = outC | 0x10;
+			case 2:
+			case 1:
+				outC = outC | 0x20;
+				break;
+		}
+		if(!(outC & 0x08)){
+			outC = outC | 0x40;
+		}
+		PORTC = outC;
+	}	
+		
+	return 1;
 }
