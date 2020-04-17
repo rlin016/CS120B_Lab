3f85a927fe5218c40ca277d834be3cbc4534693e
Raquel Lin <rlin016@wch136-11.cs.ucr.edu>
2020-04-16 23:40:52 -0700

    finished part 1

--- Lab4/source/main.c
+++ Lab4/source/main.c
@@ -15,61 +15,69 @@
 
 enum States{Start, ZeroOn, ZeroPress, OneOn, OnePress} state;
 
-void Tick(){
-	switch(state){
-		case Start: 
-			state = ZeroOn; 
-			break;
-		case ZeroOn:
-			if(A0){
-				state = ZeroPress;
-			}
-			break;
-		case ZeroPress:
-			if(!A0){
-				state = OneOn;
-			}
-			break;
-		case OneOn:
-			if(A0){
-				state = OnePress;
-			}
-			break;
-		case OnePress:
-			if(!A0){
-				state = ZeroOn;
-			}
-			break;
-		default:
-			state = ZeroOn;
-			break;
-	}
-	switch(state){
-		case ZeroOn:
-			B = 0x01;
-			break;
-		case ZeroPress:
-			B = 0x01;
-			break;
-		case OneOn:
-			B = 0x02;
-			break;
-		case OnePress:
-			B = 0x02;
-			break;
-		default: 
-			B = 0x00;
-			break;
-	}
-};
+void Tick();
 
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0xFF; PINA = 0x00;
-	DDRB = 0x00; PORTB = 0x00; 
+	DDRB = 0x00; PORTB = 0x00;
+//	unsigned char tempA, tempB;
     /* Insert your solution below */
     while (1) {
 	Tick();
     }
     return 1;
 }
+
+void Tick(){
+//	DDRA = 0xFF; PINA = 0x00;
+//	DDRB = 0x00; PORTB = 0x00;
+	unsigned tempA, tempB;
+	tempA = PINA;
+	tempB = PORTB;
+	switch(state){
+
+		state = ZeroOn;
+		break;
+	case ZeroOn:
+		if(tempA & 0x01){
+			state = ZeroPress;
+		}
+		break;
+	case ZeroPress:
+		if(!(tempA & 0x01)){
+			state = OneOn;
+		}
+		break;
+	case OneOn:
+		if(tempA & 0x01){
+			state = OnePress;
+		}
+		break;
+	case OnePress:
+		if(!(tempA & 0x01)){
+			state = ZeroOn;
+		}
+		break;
+	default:
+		state = ZeroOn;
+		break;
+	}
+	switch (state){
+		case ZeroOn:
+			tempB = 0x01;
+			break;
+		case ZeroPress:
+			tempB = 0x01;
+			break;
+		case OneOn:
+			tempB = 0x02;
+			break;
+		case OnePress:
+			tempB = 0x02;
+		default:
+			tempB = 0x00;
+			break;
+	}
+	PORTB = tempB;
+};
