f34d2b3f5a18a6c7b0760b6a681288a6405406dc
rlin016 <rlin016@ucr.edu>
2020-04-25 20:15:37 -0700

    wrote the tests for parts 1 and 2

--- Lab5/source/main.c
+++ Lab5/source/main.c
@@ -10,103 +10,47 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-//#include "RIMS.h"
 #endif
 
-enum States{Start, Wait, UpPress, UpRelease, DownPress, DownRelease, Reset} state;
-unsigned char tempA, tempC;
-void Tick();
-
 int main(void) {
+    /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
-
-	state = Start;
-	tempC = 0x00;
-    while (1){
-	tempA = ~PINA;
-	Tick();
-	PORTC = tempC;
-    }
-    return 1;
-}
-
-void Tick(){
-//	tempA = PINA;
-//	tempB = 0x00;
+    /* Insert your solution below */
+	unsigned tempA, outC;
 	
-//	tempB = tempA;
-	switch(state){
-		case Start:
-			state = Wait;
-			break;
-		case Wait:
-			if((tempA & 0x01) == 0x01){
-				state = UpPress;
+	while(1){
+		tempA = (~PINA) & 0x0F;
+		outC = 0x00;
+		switch (tempA){
+			case 15:
+			case 14:
+			case 13:
+				outC = outC | 0x01;
+			case 12:
+			case 11:
+			case 10:
+				outC = outC | 0x02;
+			case 9:
+			case 8:
+			case 7:
+				outC = outC | 0x04;
+			case 6:
+			case 5:
+				outC = outC | 0x08;
+			case 4:
+			case 3:
+				outC = outC | 0x10;
+			case 2:
+			case 1:
+				outC = outC | 0x20;
 				break;
-			}
-			else if((tempA & 0x02) == 0x02){
-				state = DownPress;
-				break;
-			}
-			else if((tempA & 0x03) == 0x03){
-				state = Reset;
-				break;
-			}
-			break;
-		case UpPress:
-			if((tempA & 0x03) == 0x03){
-				state = Reset;
-				break;
-			}
-			state = UpRelease;
-			break;
-		case UpRelease:
-			if((tempA & 0x03) == 0x03){
-				state = Reset;
-			}
-			else if(!(tempA & 0x01)){
-				state = Wait;
-			}
-			break;
-		case DownPress:
-			if((tempA & 0x03) == 0x03){
-				state = Reset;
-				break;
-			}
-			state = DownRelease;
-			break;
-		case DownRelease:
-			if((tempA & 0x03) == 0x03){
-				state = Reset;
-			}
-			else if(!(tempA & 0x01)){
-				state = Wait;
-			}
-			break;
-		case Reset:
-			state = Wait;
-			break;
-
-	}
-	switch (state){
-		case Start:
-		case Wait:
-		case UpRelease:
-		case DownRelease:
-			break;
-		case Reset:
-			tempC = 0;
-			break;
-		case UpPress:
-			if(tempC < 9){
-				tempC = tempC + 1;
-			}
-			break;
-		case DownPress:
-			if(tempC > 0){
-				tempC = tempC - 1;
-			}
-			break;		
-	}
-};
+		}
+		if(!(outC & 0x08)){
+			outC = outC | 0x40;
+		}
+		PORTC = outC;
+	}	
+		
+	return 1;
+}
