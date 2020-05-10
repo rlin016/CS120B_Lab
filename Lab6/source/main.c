18ae55a08d765ca70f4df36f2e3c5d2978d997f6
rlin016 <rlin016@ucr.edu>
2020-05-10 01:49:11 -0700

    figured out I was int he wrong port.. again part 3

--- Lab6/source/main.c
+++ Lab6/source/main.c
@@ -20,7 +20,7 @@ void Tick();
 
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
 	TimerSet(100);
 	TimerOn();
 	state = Start;
@@ -142,5 +142,5 @@ void Tick(){
 			break;
 				
 	}
-	PORTC = tempC;
+	PORTB = tempC;
 };
