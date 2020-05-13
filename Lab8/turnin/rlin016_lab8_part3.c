6be5ba72ac4feac02a6dd8267e1d158f46a9f75e
rlin016 <45045419+rlin016@users.noreply.github.com>
2020-05-13 01:39:28 -0700

    Create rlin016_lab8_part3.c

---
+++ Lab8/turnin/rlin016_lab8_part3.c
@@ -0,0 +1,35 @@
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#include "../header/timer.h"
+#endif
+
+void ADC_init(){
+  ADCSRA |= (1 << ADEN) | (1 << ADSC) | (! << ADATE);
+}
+
+unsigned short ADC_result;
+unsigned char tempB, tempD;
+
+void Tick();
+
+int main(void){
+  ADC_init();
+  ADC_result = 0x00;
+  tempB = 0x00;
+  tempD = 0x00;
+  DDRB = 0xFF; PORTB = 0x00;
+  DDRD = 0xFF; PORTD = 0x00;
+  DDRA = 0x00; PORTA = 0x00;
+  while(1){
+    Tick();
+  }
+  return 1;
+}
+void Tick(){
+  ADC_result = ADC;
+  tempB = (char)(ADC_result);
+  tempD = (char)((ADC_result >> 8) & 0x03));
+  PORTB = tempB;
+  PORTD = tempD;
+}
