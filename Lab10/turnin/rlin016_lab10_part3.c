b014c13c5416397042815171a09b79be16bfef07
rlin016 <rlin016@ucr.edu>
2020-05-23 02:08:16 -0700

    finished part 3

--- Lab10/turnin/rlin016_lab10_part3.c
+++ Lab10/turnin/rlin016_lab10_part3.c
@@ -16,37 +16,31 @@
 void TickThreeLEDs();
 void TickBlinkingLEDs();
 void TickCombineLEDs();
-void TickFrequencyChecker();
-void TickOutputSpeaker();
+void TickSpeaker();
 
 enum ThreeLeds{ThreeStart, ZeroLed, OneLed, TwoLed}threestate;
 enum BlinkingLeds{BlinkStart, OnLed, OffLed}blinkstate;
-enum FrequencyChecker{FreqStart, FreqWait, FreqUp, FreqDown}freqstate;
-enum OutputSpeaker{SpeakStart, SpeakWait, SpeakHigh, SpeakLow}speakstate; 
+enum SpeakerStates{SpeakStart, SpeakWait, SpeakOn, SpeakOff}speakstate;
 
-unsigned char frequency, k, speakerOutput; //increment k, assign to frequency
-unsigned char tempA, tempB;
-unsigned short i, j;
-unsigned char threeLed, blinkingLed;
+unsigned char tempA, tempB, i, j, k;
+unsigned char threeLed, blinkingLed, speakerOutput;
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
+	DDRA = 0x00; PINA = 0xFF;
     /* Insert your solution below */
 	i = 0;
 	j = 0;
-	k = 1;
-	TimerSet(1);
+	TimerSet(2);
 	TimerOn();
     while (1) {
-	    tempA = ~PINA;
+	    tempA = ~PINA & 0x04;
 	    tempB = 0; 
 	    TickThreeLEDs();
 	    TickBlinkingLEDs();
 	    TickCombineLEDs();
-	    TickFrequencyChecker();
-	    TickOutputSpeaker();
+	    TickSpeaker();
 	    PORTB = tempB;
 	    while(!TimerFlag);
 	    TimerFlag = 0;
@@ -55,7 +49,7 @@ int main(void) {
 }
 
 void TickThreeLEDs(){
-	if(i == 300){
+	if(i == 150){
 	switch(threestate){
 		case ThreeStart:
 			threestate = ZeroLed;
@@ -83,7 +77,7 @@ void TickThreeLEDs(){
 			threeLed = 0x04;
 			break;	
 	}
-		i = 0;
+	i = 0;
 	}
 	else{
 		i++;
@@ -91,7 +85,7 @@ void TickThreeLEDs(){
 }
 
 void TickBlinkingLEDs(){
-	if(j == 1000){
+	if(j == 500){
 	switch(blinkstate){
 		case BlinkStart:
 			blinkstate = OnLed;
@@ -126,85 +120,43 @@ void TickCombineLEDs(){
 	tempB = (threeLed & 0x07) | (blinkingLed & 0x08) | (speakerOutput & 0x10);
 }
 
-void TickFrequencyChecker(){
-	switch(freqstate){
-		case FreqStart:
-			freqstate = FreqWait;
-			break;
-		case FreqWait:
-			if(tempA == 0x01){
-				freqstate = FreqUp;
-			}
-			else if(tempA == 0x02){
-				freqstate = FreqDown;
-			}
-			break;
-		case FreqUp:
-			freqstate = FreqWait;
-			break;
-		case FreqDown:
-			freqstate = FreqWait;
-			break;
-	}
-	switch(freqstate){
-		case FreqStart:
-		case FreqWait:
-			break;
-		case FreqUp:
-			if(k > 1){
-				k--;
-			}
-			break;
-		case FreqDown:
-			if(k < 250){
-				k++;
-			}
-			break;
-	}
-}
-
-void TickOutputSpeaker(){
+void TickSpeaker(){
 	switch(speakstate){
 		case SpeakStart:
 			speakstate = SpeakWait;
 			break;
 		case SpeakWait:
 			if(tempA == 0x04){
-				frequency = k;
-				speakstate = SpeakHigh;
+			 	speakstate = SpeakOn;
 			}
 			break;
-		case SpeakHigh:
-			if((frequency == 0)){
-				frequency = k;
-				speakstate = SpeakLow;
-			}
-			break;
-		case SpeakLow:
-			if((frequency == 0) && (tempA == 0x04)){
-				frequency = k;
-				speakstate = SpeakHigh;
+		case SpeakOn:
+			if((tempA != 0x04)){
+				speakstate = SpeakWait;
 			}
 			else{
+				speakstate = SpeakOff;
+			}
+			break;
+		case SpeakOff:
+			if((tempA != 0x04)){
 				speakstate = SpeakWait;
+			}
+			else{
+			
+				speakstate = SpeakOn;
 			}
 			break;
 	}
 	switch(speakstate){
 		case SpeakStart:
-			break;
 		case SpeakWait:
-			frequency = k;
 			break;
-		case SpeakHigh:
+		case SpeakOn:
 			speakerOutput = 0x10;
-			frequency--;
 			break;
-		case SpeakLow:
+		case SpeakOff:
 			speakerOutput = 0x00;
-			frequency--;
 			break;
 	}
 }
-
-			
