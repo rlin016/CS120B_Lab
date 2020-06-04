d2db0508e4f1e22c3ef4a3e5aaca04586bdf1bb4
rlin016 <rlin016@ucr.edu>
2020-06-04 16:21:11 -0700

    finished lab 7 part 1

--- Lab7/source/main.c
+++ Lab7/source/main.c
@@ -1,113 +1,146 @@
-/*	Author: raquelxyz
+/*      Author: raquelxyz
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *      Lab Section:
+ *      Assignment: Lab #  Exercise #
+ *      Exercise Description: [optional - include for your own benefit]
  *
-i *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
+i *     I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#include "../header/timer.h"
-#include "../header/io.h"
+//#include "../header/io.h"
 #endif
+#include "timer.h"
+#include "io.h"
 
-enum States{Start, Wait, Increment, Decrement, Reset}state;
-unsigned char tempA, count;
+enum States{Wait, Up, UpWait, Down, DownWait, Reset} state;
 
 void Tick();
-void Display_Lights();
+void Display(unsigned char);
+
+unsigned char tempA;
+unsigned char count;
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRC = 0xFF; PORTC = 0x00;
-	DDRD = 0xFF; PORTD = 0x00;
-	DDRA = 0x00; PORTA = 0xFF;
-	TimerSet(1000);
-	TimerOn();
+        DDRC = 0xFF; PORTC = 0x00;
+        DDRD = 0xFF; PORTD = 0x00;
+        DDRA = 0x00; PORTA = 0xFF;
+
+        state = Wait;
+        count = 0;
+
+        LCD_init();
+        TimerSet(100);
+        TimerOn();
+
     /* Insert your solution below */
     while (1) {
-	    Tick();
-	    while(!TimerFlag);
-	    TimerFlag = 0;
+            tempA = ~PINA;
+            Tick();
+            while(!TimerFlag);
+            TimerFlag = 0;
+
     }
-    return 1;
+        return 1;
 }
+
+void Display(unsigned char count){
+        LCD_ClearScreen();
+        LCD_WriteData(count + '0');
+}
+
 
 void Tick(){
-	tempA = ~PINA;
-	switch(state){
-		case Start:
-			count = 0;
-			state = Wait;
-			break;
-		case Wait:
-			if((tempA & 0x03)== 0x01){
-			       state = Increment;
-			}
-			else if((tempA & 0x03) == 0x02){
-				state = Decrement;
-			}
-			else if((tempA & 0x03) == 0x03){
-				state = Reset;
-			}
-			break;
-		case Increment:
-			if((tempA & 0x03) == 0x01){
-				state = Increment;
-			}
-			else if((tempA & 0x03) == 0x03){
-				state = Reset;
-			}
-			else{
-				state = Wait;
-			}
-			break;
-		case Decrement:
-			if((tempA & 0x03) == 0x02){
-				state = Decrement;
-			}
-			else if((tempA & 0x03) == 0x03){
-				state = Reset;
-			}
-			else{
-				state = Wait;
-			}
-			break;
-		case Reset:
-			state = Wait;
-			break;
-	}
-	switch(state){
-		case Start:
-			break;
-		case Wait:
-			Display_Lights();
-			break;
-		case Increment:
-			if(count < 9){
-				count++;
-			}
-			Display_Lights();
-			break;
-		case Decrement:
-			if(count > 0){
-				count--;
-			}
-			Display_Lights();
-			break;
-		case Reset:
-			count = 0;
-			Display_Lights();
-			break;
-	}
+        static unsigned char i = 0;
+        switch(state){
+                case Wait:
+                        if((tempA & 0x03) == 0x01){
+                                state = Up;
+                        }
+                        else if((tempA & 0x03) == 0x02){
+                                state = Down;
+                        }
+                        else if((tempA & 0x03) == 0x03){
+                                state = Reset;
+                        }
+                        break;
+                case Up:
+                        if((tempA & 0x03) == 0x03){
+                                state = Reset;
+                        }
+                        else if((tempA & 0x01) == 0x00){
+                                state = Wait;
+                        }
+                        else{
+                                state = UpWait;
+                        }
+                        break;
+                case UpWait:
+                        if((tempA & 0x03) == 0x03){
+                                state = Reset;
+                        }
+                        else if ((i == 10) && ((tempA & 0x03) == 0x01)){
+                                i = 0;
+                                state = Up;
+                        }
+                        else if((tempA & 0x03) == 0){
+                                state = Wait;
+                        }
+                        break;
+                case Down:
+                        if((tempA & 0x03) == 0x03){
+                                state = Reset;
+                        }
+                        else if((tempA & 0x02) == 0x00){
+                                state = Wait;
+                        }
+                        else{
+                                state = DownWait;
+                        }
+                        break;
+                case DownWait:
+                        if((tempA & 0x03) == 0x03){
+                                state = Reset;
+                        }
+                        else if((i == 10) && ((tempA & 0x03) == 0x02)){
+                                i = 0;
+                                state = Down;
+                        }
+                        else if((tempA & 0x03) == 0){
+                                state = Wait;
+                        }
+                        break;
+                case Reset:
+                        state = Wait;
+                        break;
+        }
+        switch(state){
+                case Wait:
+                        break;
+                case Up:
+                        if(count < 9){
+                                count++;
+                        }
+                        break;
+                case UpWait:
+                case DownWait:
+                        i++;
+                        break;
+                case Down:
+                        if(count > 0){
+                                count--;
+                        }
+                        break;
+                case Reset:
+                        count = 0;
+                        break;
+        }
+
+        Display(count);
 }
 
-void Display_Lights(){
-	PORTC = count;
-	//LCD_WriteData(count + '0');
-}
-
+                                                                                                                                                         
 
